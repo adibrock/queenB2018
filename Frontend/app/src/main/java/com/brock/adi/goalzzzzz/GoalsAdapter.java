7330f504ad3cb35bc7cd3800d01a23f29fd944f9
adi <adi@barak.com>
2018-05-27 14:23:54 +0300

    Goal calss added

--- Frontend/app/src/main/java/com/brock/adi/goalzzzzz/GoalsAdapter.java
+++ Frontend/app/src/main/java/com/brock/adi/goalzzzzz/GoalsAdapter.java
@@ -4,22 +4,17 @@ import android.support.v7.widget.RecyclerView;
 
 public class GoalsAdapter {
 
-    public class MessageViewHolder extends RecyclerView.ViewHolder {
+//    public class MessageViewHolder extends RecyclerView.ViewHolder {
+//
+//        TextView message;
+//        TextView timeStamp;
+//
+//        public MessageViewHolder(View itemView) {
+//            super(itemView);
+//            message = itemView.findViewById(R.id.item_massage);
+//            timeStamp = itemView.findViewById(R.id.item_timestamp);
+//        }
+//    }
 
-        TextView message;
-        TextView timeStamp;
-
-        public MessageViewHolder(View itemView) {
-            super(itemView);
-            message = itemView.findViewById(R.id.item_massage);
-            timeStamp = itemView.findViewById(R.id.item_timestamp);
-        }
-    }
-
-    private MessageViewModel viewModel;
-
-    public MessageAdapter(MessageViewModel viewModel) {
-        this.viewModel = viewModel;
-    }
 
 }
