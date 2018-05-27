54e355fe36c101cd422fd79e07158ff8a675b03b
reem <reem.halamish@gmail.com>
2018-05-27 14:11:58 +0300

    added checks to avoid crashes

--- backend/main_backend.py
+++ backend/main_backend.py
@@ -2,6 +2,7 @@ import urllib
 import flask
 import json
 import pickle
+import json
 app = flask.Flask(__name__)
 
 
@@ -33,13 +34,21 @@ def make_new_user():
 
 @app.route("/user/<uid>/edit", methods=['POST'])
 def edit_user(uid):
+    if uid not in users_dict:
+        return "user not found"
+
     if flask.request.json is not None and "username" in flask.request.json:
         users_dict[uid].name = flask.request.json["username"]
         return "success"
     else:
         return "something went wrong in edit_user"
 
+@app.route("/user/<uid>/get")
+def get_user(uid):
+    if uid not in users_dict:
+        return "user not found"
 
+    return json.dumps(users_dict[uid])
 
 
 if __name__ == "__main__":
