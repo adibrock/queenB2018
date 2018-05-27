2f768255f6b598d0140ab53c45bebac5f01c99a4
Elyasaf <elyasafc@gmail.com>
2018-05-27 12:59:05 +0300

    more advancements

--- backend/main_backend.py
+++ backend/main_backend.py
@@ -1,27 +1,31 @@
 import urllib
 import flask
+import json
+import pickle
 app = flask.Flask(__name__)
 
-from urllib import request
-# @app.route('/home/')
-# def function():
-#     return 'Hello World!'
 
+users_dict = {}
 
 class User:
     lastId = -1
-    users_dict = {}
+
 
     def __init__(self):
         self.id = str(User.lastId + 1)
         User.lastId += 1
-        User.users_dict[self.id] = self
+        users_dict[self.id] = self
 
     def getId(self):
         return self.id
 
+class Goal:
+
+    def __init__(self):
+        pass
+
 @app.route('/user/newId')
-def make_new_user(__name__):
+def make_new_user():
     new_user = User()
     id = new_user.getId()
     str_id = id
@@ -29,8 +33,13 @@ def make_new_user(__name__):
 
 @app.route("/user/<uid>/edit", methods=['POST'])
 def edit_user(uid):
-    if request.json is not None and "username" in request.json:
-        User.users_dict[uid].name = request.json["username"]
+    if flask.request.json is not None and "username" in flask.request.json:
+        users_dict[uid].name = flask.request.json["username"]
+        return "success"
+    else:
+        return "something went wrong in edit_user"
+
+
 
 
 if __name__ == "__main__":
