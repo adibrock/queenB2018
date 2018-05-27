96103ed2068ded776e8531a7d2377b9b0fa2ccff
Elyasaf <elyasafc@gmail.com>
2018-05-27 11:55:34 +0300

    initial finish new user commands

--- backend/main_backend.py
+++ backend/main_backend.py
@@ -1,13 +1,37 @@
+import urllib
+import flask
+app = flask.Flask(__name__)
+
+from urllib import request
+# @app.route('/home/')
+# def function():
+#     return 'Hello World!'
 
 
 class User:
     lastId = -1
-    user_list = {}
+    users_dict = {}
 
-    def __init__(self, name):
-        self.name = name
-        self.id = User.lastId + 1
+    def __init__(self):
+        self.id = str(User.lastId + 1)
         User.lastId += 1
+        User.users_dict[self.id] = self
 
     def getId(self):
-        return str(self.id)
+        return self.id
+
+@app.route('/user/newId')
+def make_new_user(__name__):
+    new_user = User()
+    id = new_user.getId()
+    str_id = id
+    return str_id
+
+@app.route("/user/<uid>/edit", methods=['POST'])
+def edit_user(uid):
+    if request.json is not None and "username" in request.json:
+        User.users_dict[uid].name = request.json["username"]
+
+
+if __name__ == "__main__":
+    app.run()
